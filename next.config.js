c12faf315a2bcfbbb3cd8bab741411f7f889a5f9
Arthur Guiot <arguiot@gmail.com>
2026-06-05 07:50:21 -0700

    fix: add missing image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['guidelines.pr1mer.tech'],
+    domains: [
+      'guidelines.pr1mer.tech',
+      'images.pr1mer.tech',
+      'cdn-images-1.medium.com'
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
